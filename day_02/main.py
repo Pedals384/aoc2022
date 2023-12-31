5bbfc4d3804a44af8e1acb4fee3f8f5aa9d78f3c
Pedals384 <beth4goals@gmail.com>
2023-12-31 16:38:58 +0000

    part 1 done

--- day_02/main.py
+++ day_02/main.py
@@ -59,14 +59,43 @@ def score_wins():
     for opponent_score, my_score in zip(opponent_scores, my_scores):
         #print(opponent_scores)
         #print(my_scores)
-        if opponent_score > my_score:
-            opponent_wins.append(6)
-        elif opponent_score == my_score:
-            opponent_wins.append(3)
-            my_wins.append(3)
-        elif my_score > opponent_score:
-            my_wins.append(6)
-           # print(my_wins)
+        if opponent_score == 1:
+            if my_score == 1:
+                my_wins.append(3)
+                opponent_wins.append(3)
+            elif my_score == 2:
+                my_wins.append(6)
+            else:
+                opponent_wins.append(6)
+        elif opponent_score == 2:
+            if my_score == 1:
+                opponent_wins.append(6)
+            elif my_score == 2:
+                my_wins.append(3)
+                opponent_wins.append(3)
+            else:
+                my_wins.append(6)
+        else:
+            if my_score == 1:
+                my_wins.append(6)
+            elif my_score == 2:
+                opponent_wins.append(6)
+            else:
+                my_wins.append(3)
+                opponent_wins.append(3)
+            
+        # if opponent_score > my_score:
+        #     opponent_wins.append(6)
+        # elif opponent_score == my_score:
+        #     opponent_wins.append(3)
+        #     my_wins.append(3)
+        # elif my_score == 1 and opponent_score ==3:
+        #     my_wins.append(6)
+        # elif opponent_score == 1 and my_score == 3:
+        #     opponent_wins.append(6)
+        # elif my_score > opponent_score:
+        #     my_wins.append(6)
+        #    # print(my_wins)
     return sum(my_scores) + sum(my_wins)
 
 score_wins()
